fc8936d83a16733224a885fdb0861e319e8c252f
JGR-M <german9989@outlook.com>
2020-05-03 09:11:19 -0700

    added home and about pages

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
+
+
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// index route
+app.get('/', (req, res) => {
+    const title = 'Welcome1';
+    res.render('index', {
+        title: title
+    });
+});
+
+// about route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 const port = 5000;
 
